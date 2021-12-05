bdf8c9387279022bcda24091d3bd056915799cbc
Enji <enji@gmail.com>
2021-12-05 22:06:20 +0200

    22:06

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import {BrowserRouter, HashRouter} from 'react-router-dom';
 import {Provider} from "react-redux";
 
 ReactDOM.render(
-    <BrowserRouter>
+    <BrowserRouter basename={"/demoProjectSocialMedia"}>
         <React.StrictMode>
             <Provider store={store}>
                 <App/>
